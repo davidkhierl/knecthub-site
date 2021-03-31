c056b2568b847c0446aacfbb94a61d42c3ddeb2d
David Khierl Gidor <dkrgidor@gmail.com>
2021-04-01 00:34:02 +0400

    wip: 🚧 landing page.

---
+++ components/container.tsx
@@ -0,0 +1,19 @@
+import { BoxProps, Flex, forwardRef } from "@chakra-ui/react";
+
+import React from "react";
+
+export type ContainerProps = BoxProps;
+
+const Container = forwardRef<ContainerProps, "div">((props, ref) => (
+  <Flex
+    ref={ref}
+    flexDirection="column"
+    maxW={1680}
+    w="100%"
+    mx="auto"
+    px={4}
+    {...props}
+  />
+));
+
+export default Container;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,15 @@
+import { Box } from "@chakra-ui/layout";
+import Container from "../components/container";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Knecthub</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>Knecthub</h1>
-
-        <p className={styles.description}>Coming Soon</p>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      <Container minH="100vh">test</Container>
+    </>
   );
 }
